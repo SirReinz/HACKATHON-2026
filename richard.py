b4cc8b914bb72927ee633f9cb624c048ebc26452
Richard Peng <richardpeng2005@icloud.com>
2026-04-18 11:55:48 +1000

    converted to shadcn vite

--- richard.py
+++
@@ -1,9 +0,0 @@
-richards_favourite_number = 67
-my_sanity = 0
-
-while (my_sanity < richards_favourite_number):
-    print("Richard is a smelly poo poo <3")
-    my_sanity += 1
-    if(my_sanity == 67):
-        print("it's 6767776676767676767!!!")
-
